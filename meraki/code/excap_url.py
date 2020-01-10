fb5b7024d104a55689c49c360f0fc9d479344bc8
nick <nickrus@cisco.com>
2020-01-10 17:43:41 +0000

    need more thought on excap url testing

--- meraki/code/excap_url.py
+++ meraki/code/excap_url.py
@@ -1,14 +1,14 @@
 # https://github.com/meraki/js-splash
 import urllib.parse
 s=urllib.parse.unquote("http://localhost:5000/?base_grant_url=https%3A%2F%2Fn143.network-auth.com%2Fsplash%2Fgrant&user_continue_url=http%3A%2F%2Fspeedof.me%2F&node_id=149624922840090&node_mac=88:15:44:60:1c:1a&gateway_id=149624922840090&client_ip=10.255.60.208&client_mac=f4:5c:89:9b:17:67")
-print(s)
+#print(s)
 
 # http://localhost:5000/?base_grant_url=https://n143.network-auth.com/splash/grant&user_continue_url=http://speedof.me/&node_id=149624922840090&node_mac=88:15:44:60:1c:1a&gateway_id=149624922840090&client_ip=10.255.60.208&client_mac=f4:5c:89:9b:17:67
 
 url = "http://52.206.215.125/index.html"
 params = {
     "base_grant_url": "https://n143.network-auth.com/splash/grant",
-    "user_continue_url": "http://speedof.me/",
+    "user_continue_url": "http://njrusmc.net/",
     "node_id": 149624922840090,
     "node_mac": "88:15:44:60:1c:1a",
     "gateway_id": 149624922840090,
@@ -18,6 +18,6 @@ params = {
 
 import requests
 resp = requests.get(url, params=params)
-print(resp)
-breakpoint()
-print(resp)
+# print(resp)
+# print(resp.text)
+print(resp.request.url)

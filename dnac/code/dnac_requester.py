486b543d477d158d9898db5bbf5a2c0241b1b1a4
Nicholas Russo <njrusmc@gmail.com>
2020-01-12 09:48:47 -0500

    dnac event notes/samples

--- dnac/code/dnac_requester.py
+++ dnac/code/dnac_requester.py
@@ -15,7 +15,7 @@ class DNACRequester:
     Lightweight SDK class to simplify interacting with the DNAC REST API.
     """
 
-    def __init__(self, host, username, password, verify=True):
+    def __init__(self, host, username, password, verify=True, old_style=False):
         """
         Constructor to build a new object. Automatically collects an
         authorization token and assembles the headers used for all
@@ -37,9 +37,12 @@ class DNACRequester:
         }
 
         # Get token, then update headers dict
-        auth_resp = self.req(
-            "dna/system/api/v1/auth/token", method="post", auth=(username, password)
-        )
+        if old_style:
+            auth_url = "api/system/v1/auth/token"
+        else:
+            auth_url = "dna/system/api/v1/auth/token"
+
+        auth_resp = self.req(auth_url, method="post", auth=(username, password))
         auth_resp.raise_for_status()
         self.headers["X-Auth-Token"] = auth_resp.json()["Token"]
 
